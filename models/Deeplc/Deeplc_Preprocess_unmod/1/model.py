25b2c9f407af96cb681aaafffb9994801b2c9613
WassimG <wassim.gabriel@gmail.com>
2023-01-18 16:13:49 +0100

    fix batch dimension

--- models/Deeplc/Deeplc_Preprocess_unmod/1/model.py
+++ models/Deeplc/Deeplc_Preprocess_unmod/1/model.py
@@ -29,7 +29,7 @@ class TritonPythonModel:
       peptides_ = peptide_in.as_numpy().tolist()
       peptide_in_list = [x[0].decode('utf-8')  for x in peptides_ ]
 
-      sequences = np.asarray(internal_without_mods(peptide_in_list))
+      sequences = np.asarray([internal_without_mods(peptide_in_list)])
       t = pb_utils.Tensor("stripped_peptide",sequences.astype(np.object_))
       responses.append(pb_utils.InferenceResponse(output_tensors=[t]))
       print("sequences: ")
