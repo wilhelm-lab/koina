eb44b2d7f72e8f26ab0b46ad0540c2b8e1d0eb55
LLautenbacher <59884643+LLautenbacher@users.noreply.github.com>
2023-02-01 13:05:50 +0100

    Ci pipeline (#15)
    Set up CI to automatically create a semantic versioning tag & create a docker image on merge to main.

--- getModels.sh
+++ getModels.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/bash
 for file in $(find . -name '.zenodo')
 do 
 	folder=${file::-8}
 	tf=$folder/tmp.zip
-	wget --content-disposition -i $file -O $tf
+	wget -nv --content-disposition -i $file -O $tf
 	unzip $tf -d $folder
 	rm $tf
 done
