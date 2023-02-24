f99554a10a50d641b0324adda53bb40d90c0e434
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-02-24 10:13:28 +0000

    Add Prosit_2019_irt_ensemble_model

---
+++ models/Prosit/Prosit_2019_irt_postprocess/1/model.py
@@ -0,0 +1,33 @@
+import json
+import triton_python_backend_utils as pb_utils
+
+
+class TritonPythonModel:
+    def __init__(self):
+        super().__init__()
+        self.output_dtype = None
+        self.logger = pb_utils.Logger
+
+    def initialize(self, args):
+        model_config = json.loads(args["model_config"])
+        output0_config = pb_utils.get_output_config_by_name(
+            model_config, "out/irt_norm"
+        )
+        self.output_dtype = pb_utils.triton_string_to_numpy(output0_config["data_type"])
+
+    def execute(self, requests):
+        responses = []
+        for request in requests:
+            irt_raw = pb_utils.get_input_tensor_by_name(
+                request, "in/irt_raw"
+            ).as_numpy()
+
+            irt_norm = irt_raw * 43.39373 + 56.35363441
+
+            output_tensors = [
+                pb_utils.Tensor("out/irt_norm", irt_norm.astype(self.output_dtype))
+            ]
+
+            responses.append(pb_utils.InferenceResponse(output_tensors=output_tensors))
+
+        return responses
