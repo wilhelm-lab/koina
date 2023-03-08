0d06c022a7434cca9f8278576743697403c10b6f
Dulguun Bold <dulguunb@protonmail.com>
2023-03-08 22:12:12 +0100

    Add notes.yaml for the models. Notes hold metadata information. Refactor the swagger.yml generator script

--- swagger/swagger_gen.py
+++ swagger/swagger_gen.py
@@ -9,19 +9,11 @@ import json
 import yaml
 import time
 
-def get_example(model_names,function):
-    example = {}
-    for model_name in model_names:
-        with open(os.path.join('examples',model_name,function+".json"),'r') as json_file:
-            example[model_name] = json.load(json_file)[model_name]
-    return example
-
-def get_notes(model_names_path):
-    example = {}
-    for model_name in model_names:
-        with open(os.path.join(model_note_path),'r') as json_file:
-            example[model_name] = json.load(json_file)[model_name]
-    return example
+def get_notes(model_name,model_name_path):
+    notes = {}
+    with open(os.path.join(model_name_path,"notes.yaml"),'r') as yaml_file:
+        notes[model_name] = yaml.load(yaml_file,Loader=yaml.FullLoader)
+    return notes
 
 def remove_type_prefix(models):
     for model in models:
@@ -31,20 +23,81 @@ def remove_type_prefix(models):
             )
 
     return models
+def generate_intensity_python_code(model,server_url):
+    model_name = model['name']
+    python_code = '''
+    <pre>
+    import numpy as np <br>
+    import numpy as np <br>
+    import time <br>
+    import tritonclient.grpc as grpcclient <br>
+    if __name__ == \"__main__\" <br>
+    <blockquote> server_url = \"{server_url}\"<br>
+    model_name = \"{model_name}\"<br>
+    '''
+    python_code = python_code.format(server_url=server_url,model_name=model_name)
+    for i in range(0,len(model['output'])):
+        model_output = model['output'][i]
+        index=str(i)
+        model_output_name = model_output['name']
+        out_layer='''out_layer{index}=\"{model_output_name}\"<br>'''.format(index=index,model_output_name=model_output_name)
+        python_code+=out_layer
+
+    python_code+='''
+    batch_size = 5<br>
+    inputs = []<br>
+    triton_client = grpcclient.InferenceServerClient(url=server_url)<br>
+    '''
+    
+    for i in range(0,len(model['input'])):
+        data_type = model['input'][i]['data_type']
+        input_tensor = model['input'][i]['name']
+        input_code='''inputs.append(grpcclient.InferInput(\"{input_tensor}\",[batch_size, 1],{data_type}))<br>'''.format(input_tensor=input_tensor,data_type=data_type)
+        python_code+=input_code
+    python_code+="# Create the data for the two input tensors. Initialize the first \ n# to unique integers and the second to all ones. <br>" 
+
+    python_code+= '''    
+    peptide_seq_in = np.array([[\"AAAAAKAKM[UNIMOD:35]\"] for i in range(0, batch_size)], dtype=np.object_)<br>"
+    ce_in = np.array([[25] for i in range(0, batch_size)], dtype=np.float32)<br>
+    precursor_charge_in = np.array([[2] for i in range(0, batch_size)], dtype=np.int32)<br>
+    print("len: " + str(len(inputs)))<br>
+    inputs[0].set_data_from_numpy(peptide_seq_in)<br>
+    inputs[1].set_data_from_numpy(ce_in)<br>
+    inputs[2].set_data_from_numpy(precursor_charge_in)<br>
+    '''
+    out="outputs = ["
+    for i in range(0,len(model['output'])):
+        outputs='''grpcclient.InferRequestedOutput(out_layer{index}), '''.format(index=i)
+        out+=outputs
+    out+="]<br>"
+    python_code+=out
+    python_code+='''start = time.time()<br>
+    result = triton_client.infer(model_name, inputs=inputs, outputs=outputs)<br>
+    end = time.time()<br>
+    print(\"Time: \" + str(end - start))<br>
+    print("Result")<br>
+    print(np.round(result.as_numpy(out_layer1), 1))<br>
+    print(np.round(result.as_numpy(out_layer2), 1))<br>
+    \t  </pre>
+    '''
+    return python_code
+   
 
 def main():
     logging.basicConfig(encoding='utf-8', level=logging.INFO)
     with open('./model_names.json','r') as f:
         model_names = json.load(f)
+    print(model_names[0])
     serving_started = False
     wait_time = 10
     reverse_host = os.getenv("TRITON_SERVER_IP")
     reverse_port = os.getenv("TRITON_REVERSE_PROXY_SERVER_PORT")
 
-    triton_url_template = "http://reverse-proxy:" + reverse_port + "/v2/models/{model}/config"
+    triton_url_template = "http://" +reverse_host+ ":" + reverse_port + "/v2/models/{model}/config"
 
     while (not serving_started):
-        url = triton_url_template.format(model=model_names[0])
+        key,val = list(model_names[0].items())[0]
+        url = triton_url_template.format(model=key)
         logging.info("Waiting for serving to start: " + url)
         try:
             r = requests.get(url)
@@ -58,21 +111,32 @@ def main():
 
     models = []
     for model in model_names:
-        url = triton_url_template.format(model=model)
+        key,_ = list(model.items())[0]
+        url = triton_url_template.format(model=key)
         logging.info("The selected triton back-end to generate swagger from: " + url)
         r = requests.get(url)
         models.append(r.json())
-
+    logging.info("Models: ")
+    logging.info(models)
     environment = Environment(loader=FileSystemLoader("./"))
     template = environment.get_template("swagger_tmpl.yml")
 
-    example_output = get_example(model_names,function='output')
-    example_input = get_example(model_names,function='input')
-    context = {"models": models, "example_output": example_output,'example_input': example_input,'triton_server_ip':reverse_host,'triton_server_port':reverse_port}
+    for model in model_names:
+        name,model_path = list(model.items())[0]
+        notes = get_notes(name,model_path)
+        for i in range(0,len(models)):
+            if (models[i]['name'] == name):
+                models[i]['note'] = notes[name]
+                if ("intensity" in name):
+                    code = generate_intensity_python_code(models[i],reverse_host+":"+reverse_port)
+                    models[i]['note']['code'] = code
+
+    context = {'models': models,'triton_server_ip':reverse_host,
+              'triton_server_port':reverse_port}
 
     content = template.render(context)
     logging.info("Generating the Swagger YAML file ... ")
-    with open("swagger_test.yml", mode="w", encoding="utf-8") as yaml:
+    with open("swagger.yml", mode="w", encoding="utf-8") as yaml:
         yaml.write(content)
     logging.info("Finished Generating the Swagger YAML file ... ")
 
