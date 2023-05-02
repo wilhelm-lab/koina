f1069c0854203f1057d243cfc2b31b9968b8b692
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-05-02 15:36:24 +0000

    Add basic availability tests for Deeplc

---
+++ test/Deeplc/test_Deeplc_rt.py
@@ -0,0 +1,17 @@
+from server_config import SERVER_GRPC, SERVER_HTTP
+import tritonclient.grpc as grpcclient
+from pathlib import Path
+import requests
+
+# To ensure MODEL_NAME == test_<filename>.py
+MODEL_NAME = Path(__file__).stem.replace("test_", "")
+
+
+def test_available_http():
+    req = requests.get(f"{SERVER_HTTP}/v2/models/{MODEL_NAME}", timeout=1)
+    assert req.status_code == 200
+
+
+def test_available_grpc():
+    triton_client = grpcclient.InferenceServerClient(url=SERVER_GRPC)
+    assert triton_client.is_model_ready(MODEL_NAME)
