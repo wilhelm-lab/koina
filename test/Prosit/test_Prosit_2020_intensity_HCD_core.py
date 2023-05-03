8fce6da09809a288b55a1e2eff642e1e1b97d223
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-05-03 14:22:52 +0000

    Add test for inference accuracy of Prosit_2020_intensity_HCD

--- test/Prosit/test_Prosit_2020_intensity_HCD_core.py
+++ test/Prosit/test_Prosit_2020_intensity_HCD_core.py
@@ -17,3 +17,37 @@ def test_available_grpc():
     triton_client = grpcclient.InferenceServerClient(url=SERVER_GRPC)
     assert triton_client.is_model_ready(MODEL_NAME)
 
+def test_inference():
+    seq = np.load("test/Prosit/arr_Prosit_2019_intensity_seq.npy")
+    charge = np.load("test/Prosit/arr_Prosit_2019_intensity_charge.npy")
+    ces = np.load("test/Prosit/arr_Prosit_2019_intensity_ces.npy")
+
+    triton_client = grpcclient.InferenceServerClient(url=SERVER_GRPC)
+
+    in_pep_seq = grpcclient.InferInput("peptides_in:0", seq.shape, "INT32")
+    in_pep_seq.set_data_from_numpy(seq)
+
+    in_charge = grpcclient.InferInput("precursor_charge_in:0", charge.shape, "FP32")
+    in_charge.set_data_from_numpy(charge)
+
+    in_ces = grpcclient.InferInput("collision_energy_in:0", ces.shape, "FP32")
+    in_ces.set_data_from_numpy(ces)
+
+    result = triton_client.infer(
+        MODEL_NAME,
+        inputs=[in_pep_seq, in_charge, in_ces],
+        outputs=[
+            grpcclient.InferRequestedOutput("out/Reshape:0"),
+        ],
+    )
+
+    intensities = result.as_numpy("out/Reshape:0")
+
+    assert intensities.shape == (5, 174)
+
+    assert np.allclose(
+        intensities,
+        np.load("test/Prosit/arr_Prosit_2020_intensity_hcd_int_raw.npy"),
+        rtol=0,
+        atol=1e-4,
+    )
