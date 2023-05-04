5442fc55bc29cb0e8feaf5f503eda9f9169b8d7a
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-05-04 09:58:25 +0000

    Add inference test for Prosit_2020_irt_TMT

--- test/Prosit/test_Prosit_2020_irt_TMT_core.py
+++ test/Prosit/test_Prosit_2020_irt_TMT_core.py
@@ -17,3 +17,29 @@ def test_available_grpc():
     triton_client = grpcclient.InferenceServerClient(url=SERVER_GRPC)
     assert triton_client.is_model_ready(MODEL_NAME)
 
+def test_inference():
+    seq = np.load("test/Prosit/arr_Prosit_2019_intensity_seq.npy")
+
+    triton_client = grpcclient.InferenceServerClient(url=SERVER_GRPC)
+
+    in_pep_seq = grpcclient.InferInput("peptides_in:0", seq.shape, "INT32")
+    in_pep_seq.set_data_from_numpy(seq)
+
+    result = triton_client.infer(
+        MODEL_NAME,
+        inputs=[in_pep_seq],
+        outputs=[
+            grpcclient.InferRequestedOutput("prediction/BiasAdd:0"),
+        ],
+    )
+
+    irt = result.as_numpy("prediction/BiasAdd:0")
+
+    assert irt.shape == (5, 1)
+
+    assert np.allclose(
+        irt,
+        np.load("test/Prosit/arr_Prosit_2020_irt_TMT_irt.npy"),
+        rtol=0,
+        atol=1e-4,
+    )
