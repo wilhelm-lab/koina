eb128152f916d551870a04facfb560687b1c2eec
Elias Kahl <contact@elias.works>
2023-06-02 11:54:49 +0200

    add koina logo

--- web/components/TheHeader.vue
+++ web/components/TheHeader.vue
@@ -2,7 +2,13 @@
   <header
     class="py-4 px-2 md:px-4 lg:px-8 flex items-center justify-between font-semibold border-b-2 border-gray-200"
   >
-    <nuxt-link to="/" class="text-3xl font-bold">Koina</nuxt-link>
+    <nuxt-link
+      to="/"
+      class="flex justify-center items-center text-3xl font-bold"
+    >
+      <img src="~/assets/img/koina-logo.svg" class="h-12" />
+      <span class="ml-2"> Koina </span>
+    </nuxt-link>
     <nav>
       <ul class="flex items-center">
         <li class="ml-4">
