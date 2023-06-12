b04323b4c6285a144e2ae3a7cd9b1e7a8f3a9f26
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-06-12 12:37:19 +0000

    Update performance analyzer script.

--- docs/perf_analyzer/perfTest.sh
+++ docs/perf_analyzer/perfTest.sh
@@ -1,7 +1,7 @@
-N=13
+N=1
 (
  for p in $(cat models.txt); do
     ((i=i%N)); ((i++==0)) && wait
-    perf_analyzer -u 'serving:8501' -m $p --concurrency-range 1 -b 10 --input-data /workspace/koina/docs/perf_analyzer/input.json --shape 'peptide_sequences:1' > log/$p.log 2>&1 &
+    perf_analyzer --measurement-interval 50000 -u 'serving:8501' -m $p --concurrency-range 50 -b 1000 --input-data /workspace/koina/docs/perf_analyzer/input.json --shape 'peptide_sequences:1' > log/$p.log 2>&1 &
  done 
 )
