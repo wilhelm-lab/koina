8a2b1b8c01d266163d65e3fdff45c9678ae8788d
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-06-14 13:43:32 +0000

    Update prometheus.

--- prometheus/up.sh
+++ prometheus/up.sh
@@ -1,5 +1,6 @@
 docker run -d \
-    -v /home/user02/koina/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+	-v $(realpath prometheus/prometheus.yml):/etc/prometheus/prometheus.yml \
     --name prometheus \
-    --network host \
-    prom/prometheus --web.listen-address=:8501 --config.file=/etc/prometheus/prometheus.yml
+    -p 9090:9090 \
+    --rm \
+    prom/prometheus --config.file=/etc/prometheus/prometheus.yml
