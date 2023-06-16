247e4d56c43c15bb5e46debc17429544fd97ac03
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2023-06-16 15:27:06 +0000

    Adjust prometheus container configuration to persist queried data

--- prometheus/up.sh
+++ prometheus/up.sh
@@ -1,6 +1,6 @@
 docker run -d \
-	-v $(realpath prometheus/prometheus.yml):/etc/prometheus/prometheus.yml \
+    -v $(realpath prometheus/prometheus.yml):/etc/prometheus/prometheus.yml \
+    -v /var/log/prometheus/data:/prometheus/data \
     --name prometheus \
     -p 9090:9090 \
-    --rm \
     prom/prometheus --config.file=/etc/prometheus/prometheus.yml
