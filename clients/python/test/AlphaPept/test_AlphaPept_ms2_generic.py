e0cefa572beea23f02289e15853590fcb8a08818
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-02-27 12:36:11 +0000

    Refactor code to remove duplicate tests.

--- clients/python/test/AlphaPept/test_AlphaPept_ms2_generic.py
+++ clients/python/test/AlphaPept/test_AlphaPept_ms2_generic.py
@@ -1,9 +1,10 @@
 from test.server_config import SERVER_GRPC, SERVER_HTTP
-from koinapy import Koina
-import numpy as np
-import requests
 from pathlib import Path
-from glob import glob
+from test.lib import (
+    lib_test_available_grpc,
+    lib_test_available_http,
+    lib_test_inference,
+)
 
 
 # To ensure MODEL_NAME == test_<filename>.py
@@ -11,29 +12,12 @@ MODEL_NAME = Path(__file__).stem.replace("test_", "")
 
 
 def test_available_http():
-    req = requests.get(f"{SERVER_HTTP}/v2/models/{MODEL_NAME}", timeout=1)
-    assert req.status_code == 200
+    lib_test_available_http(MODEL_NAME, SERVER_HTTP)
 
 
 def test_available_grpc():
-    client = Koina(MODEL_NAME, server_url=SERVER_GRPC, ssl=False)
-    assert client._is_model_ready() is None
+    lib_test_available_grpc(MODEL_NAME, SERVER_GRPC)
 
 
 def test_inference():
-    files = glob(f"**/arr-{MODEL_NAME}-*", recursive=True)
-    data = {Path(f).stem.split("-")[-1]:np.load(f) for f in files}
-
-    client = Koina(MODEL_NAME, server_url=SERVER_GRPC, ssl=False)
-    preds = client.predict(data)
-    
-    for k in preds.keys():
-        try:
-            assert np.allclose(
-                preds[k],
-                data[k],
-                rtol=0,
-                atol=1e-6,
-            )
-        except TypeError:
-            assert np.all(preds[k] == preds[k])
+    lib_test_inference(MODEL_NAME, SERVER_GRPC)
