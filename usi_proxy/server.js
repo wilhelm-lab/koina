cf6bafebf36cdcf8fcb4b90f0d790afe910c6b70
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-03-12 14:23:27 +0000

    Create input dynamically.

--- usi_proxy/server.js
+++ usi_proxy/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const https = require('https');
 const url = require('url');
+const axios = require('axios');
 
 // Create Express application
 const app = express();
@@ -10,51 +11,98 @@ const PORT = 3000; // Port on which the proxy server will listen
 
 // Function to handle proxy requests
 const handleProxyRequest = (req, res, targetURL) => {
-    // Determine whether to use HTTP or HTTPS
-    const proxyRequest = (targetURL.protocol === 'https:') ? https.request : http.request;
-    const proxyReq = proxyRequest(targetURL, (proxyRes) => {
-        console.log('Response status code:', proxyRes.statusCode);
+  // Determine whether to use HTTP or HTTPS
+  const proxyRequest = (targetURL.protocol === 'https:') ? https.request : http.request;
+  const proxyReq = proxyRequest(targetURL, (proxyRes) => {
+    console.log('Response status code:', proxyRes.statusCode);
 
-        // Log data received from the target server
-        let responseData = '';
-        proxyRes.on('data', (chunk) => {
-            responseData += chunk;
-        });
-
-        proxyRes.on('end', () => {
-            console.log('Response data:', responseData);
-            res.writeHead(proxyRes.statusCode, proxyRes.headers);
-            res.end(responseData); // End the client response
-        });
+    // Log data received from the target server
+    let responseData = '';
+    proxyRes.on('data', (chunk) => {
+      responseData += chunk;
     });
 
-    // Handle errors
-    proxyReq.on('error', (err) => {
-        console.error('Proxy error:', err);
-        res.status(500).send('Proxy error');
+    proxyRes.on('end', () => {
+      console.log('Response data:', responseData);
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      res.end(responseData); // End the client response
     });
+  });
 
-    // Pipe the incoming request body to the proxy request (if any)
-    req.pipe(proxyReq);
+  // Handle errors
+  proxyReq.on('error', (err) => {
+    console.error('Proxy error:', err);
+    res.status(500).send('Proxy error');
+  });
+
+  // Pipe the incoming request body to the proxy request (if any)
+  req.pipe(proxyReq);
 };
 
+function transformKoinaSpectrum(spectrum) {
+  const mzs = spectrum.outputs.find(output => output.name === 'mz').data || [];
+  const intensities = spectrum.outputs.find(output => output.name === 'intensities').data || [];
+
+  return {
+    // attributes: [],
+    mzs: mzs.filter((mz, index) => intensities[index] > 1e-4),
+    intensities: intensities.filter((intensity) => intensity > 1e-4),
+  };
+}
+
+function usiGetInterpretation(inputString) {
+  const parts = inputString.split(':');
+  return parts.length > 5 ? parts[5] : '';
+}
+
+async function createReqInput(modelName) {
+  try {
+    const response = await axios.get(`http://serving:8501/v2/models/${modelName}/config`);
+    modelConfig = response.data; // Send the data from the URL back to the client
+  } catch (error) {
+    console.error('Failed to fetch config:', error);
+    res.status(500).send('Failed to fetch config');
+  }
+
+  return modelConfig['input'].map(({ name, data_type }) => ({
+    name,
+    datatype: data_type.replace('TYPE_', '').replace('STRING', 'BYTES'),
+    shape: [1, 1]
+  }));
+}
+
+
 // Route handler for /v2/models/*/use endpoint
-app.get('/v2/models/*/use', (req, res) => {
+app.get('/v2/models/*/use', async (req, res) => {
   // Extract the wildcard (*) part from the URL
   const modelName = req.params[0];
-  const peptideSequence = req.query.peptide_sequence;
+  let peptideSequence;
+  let precursorCharge;
+  const usiInterpretation = usiGetInterpretation(req.query.usi ? req.query.usi : '');
 
+  [peptideSequence, precursorCharge] = usiInterpretation.split("/");
+  precursorCharge = parseInt(precursorCharge);
+
+  let payload = await createReqInput(modelName)
   // Define the static JSON payload
-  const payload = {
+  payloadInputs = payload.map(input => {
+    return {
+      ...input,
+      data: [
+        input.name === "peptide_sequences" && req.query[input.name] === undefined
+          ? peptideSequence
+          : input.name === "precursor_charges" && req.query[input.name] === undefined
+            ? precursorCharge
+            : input.datatype != "BYTES"
+              ? parseFloat(req.query[input.name])
+              : req.query[input.name]]
+    }
+  }
+  );
+
+  payload = {
     "id": "0",
-    "inputs": [
-      {
-        "name": "peptide_sequences",
-        "shape": [1,1],
-        "datatype": "BYTES",
-        "data": [ peptideSequence ]
-      }
-    ]
+    "inputs": payloadInputs
   };
 
   // Define options for the POST request
@@ -79,7 +127,11 @@ app.get('/v2/models/*/use', (req, res) => {
     })
     .then(data => {
       // Handle the response data as needed
-      res.json(data);
+      data = transformKoinaSpectrum(data);
+      res.json({
+        attributes: { "origin": "Koina", "model": modelName, "payload": payload },
+        ...data
+      });
     })
     .catch(error => {
       console.error('There was a problem with your fetch operation:', error);
@@ -91,12 +143,12 @@ app.get('/v2/models/*/use', (req, res) => {
 
 // Middleware for all endpoints
 app.use((req, res, next) => {
-    // Parse the request URL
-    const targetURL = url.parse('http://serving:8501' + req.url);
-    handleProxyRequest(req, res, targetURL);
+  // Parse the request URL
+  const targetURL = url.parse('http://serving:8501' + req.url);
+  handleProxyRequest(req, res, targetURL);
 });
 
 // Start the proxy server
 app.listen(PORT, () => {
-    console.log(`Proxy server listening on port ${PORT}`);
+  console.log(`Proxy server listening on port ${PORT}`);
 });
