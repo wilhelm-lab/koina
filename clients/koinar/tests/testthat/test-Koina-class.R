53defb188e21e855bf12dce2e9816befbf9e527a
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-04-05 10:31:53 +0200

    Add test for common user errors.

--- clients/koinar/tests/testthat/test-Koina-class.R
+++ clients/koinar/tests/testthat/test-Koina-class.R
@@ -1,4 +1,13 @@
 #R
+bloat_array <- function(arr, n) {
+  # Determine the new dimensions, repeating the first dimension `n` times
+  new_dim <- dim(arr)
+  new_dim[1] <- new_dim[1] * n
+  # Create an expanded array
+  expanded_array <- array(rep(arr, n), dim = new_dim)
+  return(expanded_array)
+}
+
 
 test_that("check Prosit2019 Fig1", {
   
@@ -24,4 +33,79 @@ test_that("check Prosit2019 Fig1", {
   (prediction_results$intensities |> order(decreasing = TRUE))[1:10] -> idx
 
   testthat::expect_equal(idx, ground_truth)
+})
+
+test_that("Check error: Server unavailable", {
+  expect_error(koinar::Koina$new(
+    model_name = "Prosit_2019_intensity",
+    server_url = "google.com",
+  ), "Server is not ready. Response status code: 404")
+})
+
+test_that("Check error: Model unavailable", {
+  expect_error(koinar::Koina$new(
+    model_name = "not_a_valid_model_name"
+  ), "ValueError: The specified model is not available at the server. ")
+})
+
+test_that("Check error: Missing input", {
+  
+  koina_instance <- koinar::Koina$new(
+    model_name = "Prosit_2019_intensity",
+    server_url = "koina.wilhelmlab.org:443",
+    ssl = TRUE
+  )
+  
+  input <- list(
+    peptide_sequence = array(c("LKEATIQLDELNQK"),
+                              dim = c(1, 1)),
+    collision_energies = array(c(25), dim = c(1, 1)),
+    precursor_charges = array(c(1), dim = c(1, 1))
+  )
+  
+  expect_error(koina_instance$predict(input), 
+               "Missing input\\(s\\): peptide_sequences.")
+})
+
+test_that("Check performance", {
+  
+  koina_instance <- koinar::Koina$new(
+    model_name = "Prosit_2019_intensity",
+    server_url = "koina.wilhelmlab.org:443",
+    ssl = TRUE
+  )
+  
+  input_data <- list(
+    peptide_sequences = array(c("LKEATIQLDELNQK"),
+                             dim = c(1, 1)),
+    collision_energies = array(c(25), dim = c(1, 1)),
+    precursor_charges = array(c(1), dim = c(1, 1))
+  )
+  large_input_data = lapply(input_data, bloat_array, 12345)
+  
+  start = Sys.time()
+  koina_instance$predict(large_input_data)
+  
+  expect_lt(Sys.time()-start, 15)
+})
+
+test_that("Check batching", {
+  
+  koina_instance <- koinar::Koina$new(
+    model_name = "Prosit_2019_intensity",
+    server_url = "koina.wilhelmlab.org:443",
+    ssl = TRUE
+  )
+  
+  input_data <- list(
+    peptide_sequences = array(c("LKEATIQLDELNQK"),
+                              dim = c(1, 1)),
+    collision_energies = array(c(25), dim = c(1, 1)),
+    precursor_charges = array(c(1), dim = c(1, 1))
+  )
+  large_input_data = lapply(input_data, bloat_array, 1234)
+  
+  predictions = koina_instance$predict(large_input_data)
+  
+  expect_equal(dim(predictions[["intensities"]]), c(1234,174))
 })
