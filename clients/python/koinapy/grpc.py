06421df1000c9ab517baef723b9fd6e3d35e0fe7
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-05-13 15:03:02 +0000

    Hotfix progress bar.

--- clients/python/koinapy/grpc.py
+++ clients/python/koinapy/grpc.py
@@ -517,7 +517,7 @@ class Koina:
         results = []
         data_subsets = list(self.__slice_dict(data, self.batchsize * 10))
         pbar = tqdm(
-            total=len(data_subsets) * 10,
+            total=ceil(next(iter(data.values())).shape[0] / self.batchsize),
             desc=f"{self.model_name}:",
             disable=disable_progress_bar,
         )
