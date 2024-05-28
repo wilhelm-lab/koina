1c2ca24db7f88ff2d89c802d00bb084e951bd3eb
Elias Kahl <contact@elias.works>
2024-05-29 00:48:38 +0200

    chore: init shadcn

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,5 +1,6 @@
-import type { Config } from 'tailwindcss'
-import Color from 'color'
+import type { Config } from 'tailwindcss';
+import Color from 'color';
+import animate from "tailwindcss-animate";
 
 const primary = Color('#001bc8')
 const secondary = Color('#f0ab00')
@@ -37,11 +38,46 @@ export default <Partial<Config>>{
     ...safeColorClasses,
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: customColors,
       backgroundImage: {
         'hero-pattern': "url('/images/hero-pattern.svg')",
       }
-    }
-  }
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: 0 },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: 0 },
+      },
+    } as any,
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+  prefix: "",
+  plugins: [animate],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 }
