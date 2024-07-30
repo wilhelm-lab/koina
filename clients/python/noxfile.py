ee43e34cd9d5b8cf733b699b43bbc554a198cb66
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-07-30 16:32:11 +0000

    Update testing framework.

--- clients/python/noxfile.py
+++ clients/python/noxfile.py
@@ -5,15 +5,13 @@ from nox import session
 import sys
 
 package = "koinapy"
-python_versions = ["3.8", "3.9", "3.10"]
+python_versions = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 nox.options.sessions = ("tests",)
 
 
 @session(python=python_versions)
-@nox.parametrize("trclient", ["2.23", "2.42"])
-def tests(session, trclient) -> None:
+def tests(session) -> None:
     """Runtime type checking using Typeguard."""
-    session.install(f"tritonclient[grpc]=={trclient}")
     session.install("pytest")
     session.install("requests")
     session.install(".")
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 source ~/.bashrc
+pipx install poetry
+pipx install nox
+pipx inject nox nox-poetry
+pipx ensurepath
 poetry install --with develop -C ./clients/python/
 cd web
 npm install
